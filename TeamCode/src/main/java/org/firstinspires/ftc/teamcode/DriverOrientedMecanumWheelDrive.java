bcdbc699511ef8b871409d302081d0fafd7a8acb
William <122133075+william27b@users.noreply.github.com>
2023-08-14 17:20:50 -0700

    Driver Oriented Drive

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverOrientedMecanumWheelDrive.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.generals.*;
+
+@TeleOp(name = "Driver Oriented Mecanum Wheel Drive")
+public class DriverOrientedMecanumWheelDrive extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        MecanumController mecanumController = new MecanumController(hardwareMap, RuntimeType.DRIVER_CONTROLLED_TELEOP);
+        mecanumController.calibrateIMUAngleOffset();
+
+        waitForStart();
+        while (opModeIsActive()) {
+            mecanumController.driverOrientedDrive(gamepad1);
+
+            if (gamepad1.b) {
+                mecanumController.InitIMU(hardwareMap);
+                mecanumController.calibrateIMUAngleOffset();
+            }
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/generals/MecanumController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/generals/MecanumController.java
@@ -4,43 +4,56 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 public class MecanumController {
     private DcMotor frontLeft;
     private DcMotor frontRight;
     private DcMotor backLeft;
     private DcMotor backRight;
+
+    private IMU imu;
+    private float imuAngleOffset = 0;
+
     private float driveSpeed = 1;
 
-    public void Init(HardwareMap hardwareMap) {
-        frontLeft = hardwareMap.dcMotor.get("frontLeft");
-        frontRight = hardwareMap.dcMotor.get("frontRight");
-        backLeft = hardwareMap.dcMotor.get("backLeft");
-        backRight = hardwareMap.dcMotor.get("backRight");
+    public void InitIMU(HardwareMap hardwareMap) {
+        imu = hardwareMap.get(IMU.class, "imu");
     }
 
-    public void InitDriverControlledTeleop() {
+    private void Init(HardwareMap hardwareMap) {
+        frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+        frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+
+        InitIMU(hardwareMap);
+    }
+
+    private void InitDriverControlledTeleop() {
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
     }
 
-    public void InitAutonomous() {
+    private void InitAutonomous() {
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
     }
 
-    public void InitIntelligentTeleop() {
+    private void InitIntelligentTeleop() {
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
         backRight.setDirection(DcMotor.Direction.REVERSE);
     }
 
-    public void InitIntelligentAutonomous() {
+    private void InitIntelligentAutonomous() {
         frontLeft.setDirection(DcMotor.Direction.REVERSE);
         frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
         backLeft.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -65,6 +78,14 @@ public class MecanumController {
         driveSpeed = speed;
     }
 
+    public void calibrateIMUAngleOffset() {
+        imuAngleOffset = -(float)imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+    }
+
+    public float getCalibratedIMUAngle() {
+        return (float)imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS) + imuAngleOffset;
+    }
+
     public void drive(Gamepad gamepad) {
         double x = gamepad.left_stick_x;
         double y = -gamepad.left_stick_y;
@@ -74,6 +95,27 @@ public class MecanumController {
         double frontRightPower = y - x - r;
         double backLeftPower = y - x + r;
         double backRightPower = y + x - r;
+
+        double max = Math.max(Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower)), Math.max(Math.abs(backLeftPower), Math.abs(backRightPower)));
+
+        frontLeft.setPower(frontLeftPower * driveSpeed / max);
+        frontRight.setPower(frontRightPower * driveSpeed / max);
+        backLeft.setPower(backLeftPower * driveSpeed / max);
+        backRight.setPower(backRightPower * driveSpeed / max);
+    }
+
+    public void driverOrientedDrive(Gamepad gamepad) {
+        double x = gamepad.left_stick_x;
+        double y = -gamepad.left_stick_y;
+        double r = gamepad.right_trigger - gamepad.left_trigger;
+
+        double xOriented = x * Math.cos(getCalibratedIMUAngle()) - y * Math.sin(getCalibratedIMUAngle());
+        double yOriented = x * Math.sin(getCalibratedIMUAngle()) + y * Math.cos(getCalibratedIMUAngle());
+
+        double frontLeftPower = yOriented + xOriented + r;
+        double frontRightPower = yOriented - xOriented - r;
+        double backLeftPower = yOriented - xOriented + r;
+        double backRightPower = yOriented + xOriented - r;
 
         double max = Math.max(Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower)), Math.max(Math.abs(backLeftPower), Math.abs(backRightPower)));
 
