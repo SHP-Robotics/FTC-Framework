c925e2440debd505356fdb49f135249be3d16bb8
William Speiser <williamspeiser@icloud.com>
2023-11-10 21:14:32 -0800

    monkye behavior

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug/Speed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/debug/Speed.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode.debug;
 public enum Speed {
     GEAR_SHIFT,
     PID_CONTROLLED,
+    PID_CONTROLLED_OVERRIDE,
     PID_CONTROLLED_WITH_OVERRIDE,
     NO_CHANGE
 }
