a3d87ef9d5dbdeb1a300d770ae060565c5fc5d3b
Aarav Chandrasekar <aaravc@gmail.com>
2023-12-11 22:58:18 -0800

    Drive By Command (Ayaan) Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/CommandBasedAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/CommandBasedAuto.java
@@ -3,25 +3,30 @@ package org.firstinspires.ftc.teamcode.autos;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import org.firstinspires.ftc.teamcode.BaseRobot;
+import org.firstinspires.ftc.teamcode.Constants;
+import org.firstinspires.ftc.teamcode.commands.DriveByCommand;
 import org.firstinspires.ftc.teamcode.commands.DriveCommand;
 import org.firstinspires.ftc.teamcode.commands.EncoderStraightDriveCommand;
 import org.firstinspires.ftc.teamcode.commands.EncoderTurnDriveCommand;
 import org.firstinspires.ftc.teamcode.shplib.commands.CommandScheduler;
 import org.firstinspires.ftc.teamcode.shplib.commands.RunCommand;
 import org.firstinspires.ftc.teamcode.shplib.commands.WaitCommand;
-//import org.firstinspires.ftc.teamcode.commands.DropConeCommand;
+import org.firstinspires.ftc.teamcode.shplib.controllers.FFController;
+import org.firstinspires.ftc.teamcode.shplib.controllers.PositionPID;
+import org.firstinspires.ftc.teamcode.shplib.hardware.drive.SHPRunnerMecanumDrive;
 
-//@Autonomous(preselectTeleOp = "CommandBasedTeleOp")
+@Autonomous(preselectTeleOp = "CommandBasedTeleOp")
 public class CommandBasedAuto extends BaseRobot {
-//    SHPMecanumAutoDrive autoDrive;
+    SHPRunnerMecanumDrive autoDrive;
 
     @Override
+
     public void init() {
         super.init();
-//        PositionPID pid = new PositionPID(0.15);
-//        pid.setErrorTolerance(100);
-//        autoDrive = new SHPMecanumAutoDrive(hardwareMap, kMotorNames, 0.15, 0.0, 0.0);
-//        autoDrive.enableFF(new FFController(0.01));
+        PositionPID pid = new PositionPID(0.15);
+        pid.setErrorTolerance(100);
+        autoDrive = new SHPRunnerMecanumDrive(hardwareMap, Constants.Drive.kMotorNames, 0.15, 0.0, 0.0,100);
+
     }
 
     @Override
@@ -32,20 +37,16 @@ public class CommandBasedAuto extends BaseRobot {
 
         myCommand.scheduleCommand(
                 new WaitCommand(2)
+                        .then(new DriveByCommand(autoDrive,1000))
+                        .then(new WaitCommand(2))
+                        .then(new DriveByCommand(autoDrive,500,1500,1500, 500))
 
-                        .then(new WaitCommand(0.5))
-                        .then(new EncoderStraightDriveCommand(drive,"forward",5,false))
-                        .then(new DriveCommand(drive,-0.2, 0,0,4,false))
-                        .then(new WaitCommand(5))
-                        .then(new EncoderTurnDriveCommand(drive,"cw",90))
-                        .then(new WaitCommand(5))
-                        .then(new EncoderTurnDriveCommand(drive,"ccw",90))
         );
     }
 
     @Override
     public void loop() {
         super.loop();
-//        telemetry.addData("auto drive at setpoint", autoDrive.atPositionSetpoint() ? "true" : "false");
+        telemetry.addData("auto drive at setpoint", autoDrive.atPositionSetpoint() ? "true" : "false");
     }
 }
