76b79768434b2624ff1718df8d9a0c2598b8bca5
HayEater22 <aradzilowski6996@gmail.com>
2024-02-06 20:55:33 -0800

    pld

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/CRServoTankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/CRServoTankDrive.java
@@ -18,13 +18,24 @@ public class CRServoTankDrive extends LinearOpMode {
 
         Servo leftPlow = (Servo) hardwareMap.get("leftPlow");
         Servo rightPlow = (Servo) hardwareMap.get("rightPlow");
-
+        Servo eye = (Servo) hardwareMap.get("eye");
 
         waitForStart();
 
         while (opModeIsActive() && !isStopRequested()) {
             left.setPower(-gamepad1.left_stick_y);
             right.setPower(-gamepad1.right_stick_y);
+
+            if (gamepad1.square){
+                eye.setPosition(0.5);
+            }
+            if (gamepad1.triangle){
+                eye.setPosition(0.345);
+            }
+
+            if (gamepad1.circle){
+                eye.setPosition(0.25);
+            }
             leftPlow.setPosition(gamepad1.left_trigger);
             rightPlow.setPosition(-gamepad1.left_trigger);
         }
