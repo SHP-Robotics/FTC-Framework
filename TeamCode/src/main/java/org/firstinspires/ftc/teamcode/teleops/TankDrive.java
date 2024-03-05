4ae29e9bf0eb534e764659ae3530062c466ff1c9
William Speiser <williamspeiser@icloud.com>
2024-03-05 10:29:51 -0800

    motor tank drive

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/TankDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/TankDrive.java
@@ -2,20 +2,20 @@ package org.firstinspires.ftc.teamcode.teleops;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.debug.AccumulationControlledServo;
 
 @TeleOp()
-public class CRServoTankDrive extends LinearOpMode {
+public class TankDrive extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
-        CRServo left = (CRServo) hardwareMap.get("left");
+        DcMotor left = (DcMotor) hardwareMap.get("left");
         left.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        CRServo right = (CRServo) hardwareMap.get("right");
+        DcMotor right = (DcMotor) hardwareMap.get("right");
         right.setDirection(DcMotorSimple.Direction.FORWARD);
 
         AccumulationControlledServo leftPlow = new AccumulationControlledServo.AccumulationControlledServoBuilder((Servo) hardwareMap.get("leftPlow"))
