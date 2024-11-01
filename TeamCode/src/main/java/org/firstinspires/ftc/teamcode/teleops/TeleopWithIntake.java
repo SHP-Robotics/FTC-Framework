1424715df65716241ece6a1c64b57fe16fad85e6
skurian27 <122177586+skurian27@users.noreply.github.com>
2024-11-01 12:36:35 -0700

    Making test TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/TeleopWithIntake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleops/TeleopWithIntake.java
@@ -3,15 +3,25 @@ package org.firstinspires.ftc.teamcode.teleops;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.shplib.BaseRobot;
+import org.firstinspires.ftc.teamcode.shplib.commands.RunCommand;
+import org.firstinspires.ftc.teamcode.shplib.utility.Clock;
 
 @TeleOp
 public class TeleopWithIntake extends BaseRobot {
-
+    private double debounce;
     @Override
     public void init(){
         super.init();
-
+        drive.setDefaultCommand(
+                new RunCommand(
+                        () -> drive.mecanum(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x)
+                )
+        );
     }
-
+    @Override
+    public void start(){
+        super.start();
+        debounce = Clock.now();
+    }
 
 }
