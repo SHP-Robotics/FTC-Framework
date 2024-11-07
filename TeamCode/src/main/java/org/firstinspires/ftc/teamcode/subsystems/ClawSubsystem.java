d23e807766aa3f488c72eb57a9e5684e482348d2
skurian27 <122177586+skurian27@users.noreply.github.com>
2024-11-07 14:20:50 -0800

    Making test TeleOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ClawSubsystem.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import static org.firstinspires.ftc.teamcode.shplib.Constants.Claw.kClawName;
+import static org.firstinspires.ftc.teamcode.shplib.Constants.Claw.kClose;
+import static org.firstinspires.ftc.teamcode.shplib.Constants.Claw.kOpen;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.teamcode.shplib.commands.Subsystem;
+
+public class ClawSubsystem extends Subsystem {
+    private final Servo claw;
+    private final double open;
+    private final double close;
+    public ClawSubsystem(HardwareMap hardwareMap){
+        claw = (Servo) hardwareMap.get(kClawName);
+        open = kOpen;
+        close = kClose;
+    }
+    public void open(){
+        claw.setPosition(open);
+    }
+    public void close(){
+        claw.setPosition(close);
+    }
+    public void changeClaw(){
+        if(claw.getPosition() == open){
+            close();
+        }
+        else{
+            open();
+        }
+    }
+
+}
