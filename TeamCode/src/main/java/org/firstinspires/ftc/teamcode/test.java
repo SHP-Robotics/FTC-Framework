3e6c243f775f771d799c8a3be855fc54b63e1b35
WilliamChou123 <wchou28@shschools.org>
2024-11-07 15:23:10 -0800

    Fixed Claw,Slide

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -32,8 +32,8 @@ public class test extends LinearOpMode {
 
             teleOpController.updateSpeed(gamepad1);
 
-            teleOpController.driveFieldCentric(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
-
+//            teleOpController.driveFieldCentric(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
+            teleOpController.driveFieldCentric(gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
 
             telemetry.addData("x", currentPosition.getX());
             telemetry.addData("y", currentPosition.getY());
@@ -48,41 +48,45 @@ public class test extends LinearOpMode {
             }
 
             viperslide.setPower(0);
-            wormgear.setPower(gamepad1.right_trigger);
-            wormgear.setPower(-gamepad1.left_trigger);
-
+//            wormgear.setPower((gamepad1.right_bumper ? 1: 0) - (gamepad1.left_bumper ? 1: 0));
+            wormgear.setPower((gamepad1.right_trigger)- (gamepad1.left_trigger)/5);
+            viperslide.setPower(gamepad1.right_stick_y/10);
             //arm speed constraints
-            if (gamepad1.right_stick_y > 0.5 && viperslide.getCurrentPosition() > 0
-                    && viperslide.getCurrentPosition() < 2000) {
-                viperslide.setPower(0.5);
-
-            }
+//            if (gamepad1.right_stick_y > 0.5 && viperslide.getCurrentPosition() > 0
+//                    && viperslide.getCurrentPosition() < 2000) {
+//
+//
+//            }
             //if gamepad1.right_stick_y is between 10% and 50%
-            if (gamepad1.right_stick_y > 0.1 && gamepad1.right_stick_y < 0.5
-                    && viperslide.getCurrentPosition() > 0
-                    && viperslide.getCurrentPosition() < 2000) {
-                viperslide.setPower(0.3);
-
-            }
+//            if (gamepad1.right_stick_y > 0.1 && gamepad1.right_stick_y < 0.5
+//                    && viperslide.getCurrentPosition() > 0
+//                    && viperslide.getCurrentPosition() < 2000) {
+//                viperslide.setPower(0.3);
+//
+//            }
 
             //if gamepad1.right_stick_y is between -10% and -50%
-            if (gamepad1.right_stick_y < -0.1 && gamepad1.right_stick_y > -0.5
-                    && viperslide.getCurrentPosition() > 0
-                    && viperslide.getCurrentPosition() < 2000) {
-                viperslide.setPower(-0.3);
+//            if (gamepad1.right_stick_y < -0.1 && gamepad1.right_stick_y > -0.5
+//                    && viperslide.getCurrentPosition() > 0
+//                    && viperslide.getCurrentPosition() < 2000) {
+//                viperslide.setPower(-0.3);
 
-                if (gamepad1.right_trigger > 0.4) {
-
-                    wormgear.setPower(0.3);
-
-                }
+//                if (gamepad1.right_trigger > 0.4) {
+//
+//                    wormgear.setPower(0.3);
+//
+//                }
 
                 if (gamepad1.dpad_left) {
                     claw.setPosition(0.5);
                 }
+
+            if (gamepad1.dpad_right) {
+                claw.setPosition(-0.5);
+            }
             }
 
             //telemetry.addData("Motor 0:",)
         }
     }
-}
+
