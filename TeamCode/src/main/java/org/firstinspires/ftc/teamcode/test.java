db793b38a4de292107a075594f8734c40eb9b2ee
WilliamChou123 <wchou28@shschools.org>
2024-11-14 17:09:29 -0800

    added modes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -13,7 +13,8 @@ public class test extends LinearOpMode {
     boolean slideon=false;
     double wristPos=0.0;
     double clawpos=0.0;
-
+    String mode ="Driving";
+    int maxwormgearpos=1000;
 
     @Override
     public void runOpMode() {
@@ -30,7 +31,6 @@ public class test extends LinearOpMode {
         DcMotor wormgear = hardwareMap.get(DcMotor.class, "WormGear");
         Servo claw = hardwareMap.get(Servo.class, "Claw");
 
-
         waitForStart();
 
         while (opModeIsActive()) {
@@ -46,9 +46,14 @@ public class test extends LinearOpMode {
             telemetry.addData("rotation", heading);
             telemetry.addData("viper", viperslide.getCurrentPosition());
             telemetry.addData("wormgear", wormgear.getCurrentPosition());
-            telemetry.addData("claw", wormgear.getCurrentPosition());
-            telemetry.addData("wrist", wrist.getPosition());
+            telemetry.addData("claw", clawpos);
+            telemetry.addData("wrist", wristPos);
             telemetry.addData("slideon: ", slideon);
+
+//claw close:0.3
+//claw open:0.7
+//wrist close:
+//wrist open:
 
 
 
@@ -61,68 +66,101 @@ public class test extends LinearOpMode {
 
             }
 
-            wormgear.setPower((gamepad1.b ? 0.5: 0) - (gamepad1.x ? 0.5: 0));
+            wormgear.setPower((gamepad1.b ? 0.6: 0) - (gamepad1.x ? 0.6: 0));
 
             if (gamepad1.y) {
                 viperslide.setTargetPosition(2150);
                 viperslide.setPower(0.4);
                 viperslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            }
 
+            }
+//            if (viperslide.getCurrentPosition()<=0){
+//                viperslide.setPower(0);
+//            }
+            if (viperslide.getCurrentPosition()>=2000){
+                    viperslide.setPower(0.1);
+
+
+
+            }
+            if (wormgear.getCurrentPosition()>=maxwormgearpos){
+                wormgear.setPower(0);
+
+
+
+            }
             if (gamepad1.a) {
+
                 viperslide.setTargetPosition(0);
                 viperslide.setPower(0.4);
                 viperslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
             }
 
-//
+            if (gamepad2.dpad_down){
+                viperslide.setTargetPosition(0);
+                viperslide.setPower(0.4);
+                viperslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                wormgear.setTargetPosition(0);
+                viperslide.setPower(0.4);
+                viperslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-            //arm speed constraints
-//            if (gamepad1.right_stick_y > 0.5 && viperslide.getCurrentPosition() > 0
-//                    && viperslide.getCurrentPosition() < 2000) {
-//
-//
-//            }
-            //if gamepad1.right_stick_y is between 10% and 50%
-//            if (gamepad1.right_stick_y > 0.1 && gamepad1.right_stick_y < 0.5
-//                    && viperslide.getCurrentPosition() > 0
-//                    && viperslide.getCurrentPosition() < 2000) {
-//                viperslide.setPower(0.3);
-//
-//            }
 
-            //if gamepad1.right_stick_y is between -10% and -50%
-//            if (gamepad1.right_stick_y < -0.1 && gamepad1.right_stick_y > -0.5
-//                    && viperslide.getCurrentPosition() > 0
-//                    && viperslide.getCurrentPosition() < 2000) {
-//                viperslide.setPower(-0.3);
+            }
 
-//                if (gamepad1.right_trigger > 0.4) {
-//
-//                    wormgear.setPower(0.3);
-//
-//                }
-//            if (gamepad1.left_stick_y >0.7  && gamepad1.right_stick_y <-0.7) {
-//                claw.setPosition(0.5);
-//
-//
-//            }
 
-//            if (gamepad1.x) {
-//                viperslide.setPower(0);
-//                wormgear.setPower(0);
-//
-//            }
+            if (gamepad2.left_bumper){
+                while (gamepad2.left_bumper) {
+                    // Do nothing, just wait
+                }
+                if(mode.equals("Driving")){
 
+                    mode ="Intake";
+                    viperslide.setTargetPosition(0);
+                    viperslide.setPower(0.4);
+                    viperslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                    wormgear.setTargetPosition(maxwormgearpos);
+                    wormgear.setPower(0.4);
+                    wormgear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    claw.setPosition(0.3);
+                    wrist.setPosition(1);
+                }
+                if(mode.equals("Intake")){
+                    mode ="Outtake";
+                    viperslide.setTargetPosition(2150);
+                    viperslide.setPower(0.4);
+                    viperslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+                    wormgear.setTargetPosition(100);
+                    wormgear.setPower(0.4);
+                    wormgear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    claw.setPosition(0.3);
+                    wrist.setPosition(1);
+
+                }
+                if(mode.equals("Outtake")){
+                    mode ="Driving";
+                    viperslide.setTargetPosition(0);
+                    viperslide.setPower(0.4);
+                    viperslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    wormgear.setTargetPosition(0);
+                    wormgear.setPower(0.4);
+                    wormgear.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+                    claw.setPosition(0.3);
+                    wrist.setPosition(0.5);
+                }
+
+
+
+            }
             if (gamepad1.dpad_left) {
-                clawpos+=0.05;
-                claw.setPosition(clawpos);
+
+                claw.setPosition(0.3);
                 }
 
             if (gamepad1.dpad_right) {
-                clawpos-=0.05;
-
-                claw.setPosition(clawpos);
+                claw.setPosition(0.7);
             }
 
             if (gamepad1.dpad_up) {
