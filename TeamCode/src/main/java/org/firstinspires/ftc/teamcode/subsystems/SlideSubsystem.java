706151713e24f4cc3d91b8af38df2c6f7dc1de92
skurian27 <122177586+skurian27@users.noreply.github.com>
2024-11-15 18:11:49 -0800

    Subsysteming

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/SlideSubsystem.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.shplib.commands.Subsystem;
+
+public class SlideSubsystem extends Subsystem {
+
+    public SlideSubsystem(HardwareMap hardwareMap){
+
+    }
+}
