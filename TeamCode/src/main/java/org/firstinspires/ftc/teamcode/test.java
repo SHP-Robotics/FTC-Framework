24bd685e30a47080676acc7072a47db662a9acee
LelandDanesh0263 <141520629+LelandDanesh0263@users.noreply.github.com>
2024-11-16 09:26:31 -0800

    Home Comp Day Edits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/test.java
@@ -7,6 +7,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.shprobotics.pestocore.drivebases.MecanumController;
 import com.shprobotics.pestocore.drivebases.MecanumTracker;
 import com.shprobotics.pestocore.drivebases.TeleOpController;
+import com.shprobotics.pestocore.drivebases.ThreeWheelOdometryTracker;
 import com.shprobotics.pestocore.geometries.Vector2D;
 
 @TeleOp
@@ -15,73 +16,66 @@ public class test extends LinearOpMode {
     @Override
     public void runOpMode() {
         MecanumController mecanumController = PestoFTCConfig.getMecanumController(hardwareMap);
-        MecanumTracker mecanumTracker = PestoFTCConfig.getTracker(hardwareMap);
-        TeleOpController teleOpController = PestoFTCConfig.getTeleOpController(mecanumController, mecanumTracker, hardwareMap);
+        ThreeWheelOdometryTracker threeWheelOdometryTracker = PestoFTCConfig.getTracker(hardwareMap);
+        //MecanumTracker mecanumTracker = PestoFTCConfig.getTracker(hardwareMap);
+        TeleOpController teleOpController = PestoFTCConfig.getTeleOpController(mecanumController, threeWheelOdometryTracker, hardwareMap);
 
         DcMotor viperslide = hardwareMap.get(DcMotor.class, "ViperSlide");
         DcMotor wormgear = hardwareMap.get(DcMotor.class, "WormGear");
         Servo claw = hardwareMap.get(Servo.class, "Claw");
+        DcMotor opodL = hardwareMap.get(DcMotor.class, "frontLeft");
+        DcMotor opodC = hardwareMap.get(DcMotor.class, "frontRight");
+        DcMotor opodR = hardwareMap.get(DcMotor.class, "backLeft");
 
 
         waitForStart();
 
         while (opModeIsActive()) {
-            mecanumTracker.updateOdometry();
-            Vector2D currentPosition = mecanumTracker.getCurrentPosition();
-            double heading = mecanumTracker.getCurrentHeading();
+            threeWheelOdometryTracker.updateOdometry();
+            Vector2D currentPosition = threeWheelOdometryTracker.getCurrentPosition();
+            double heading = threeWheelOdometryTracker.getCurrentHeading();
 
             teleOpController.updateSpeed(gamepad1);
 
             teleOpController.driveFieldCentric(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
 
-
-            telemetry.addData("x", currentPosition.getX());
-            telemetry.addData("y", currentPosition.getY());
-            telemetry.addData("rotation", heading);
-            telemetry.update();
-
-
-            if (gamepad1.b) {
-                mecanumTracker.reset();
-                teleOpController.resetIMU();
-
-            }
-
-            viperslide.setPower(0);
+            //viperslide.setPower(0);
             wormgear.setPower(gamepad1.right_trigger);
             wormgear.setPower(-gamepad1.left_trigger);
 
             //arm speed constraints
-            if (gamepad1.right_stick_y > 0.5 && viperslide.getCurrentPosition() > 0
-                    && viperslide.getCurrentPosition() < 2000) {
+            if (gamepad1.right_stick_y > 0.5) {
                 viperslide.setPower(0.5);
-
-            }
-            //if gamepad1.right_stick_y is between 10% and 50%
-            if (gamepad1.right_stick_y > 0.1 && gamepad1.right_stick_y < 0.5
-                    && viperslide.getCurrentPosition() > 0
-                    && viperslide.getCurrentPosition() < 2000) {
-                viperslide.setPower(0.3);
-
-            }
-
-            //if gamepad1.right_stick_y is between -10% and -50%
-            if (gamepad1.right_stick_y < -0.1 && gamepad1.right_stick_y > -0.5
-                    && viperslide.getCurrentPosition() > 0
-                    && viperslide.getCurrentPosition() < 2000) {
-                viperslide.setPower(-0.3);
-
-                if (gamepad1.right_trigger > 0.4) {
-
-                    wormgear.setPower(0.3);
-
                 }
+            //if gamepad1.right_stick_y is between 10% and 50%
+            else if (gamepad1.right_stick_y > 0.1 && gamepad1.right_stick_y < 0.5) {
+                viperslide.setPower(0.3);
+            }
+            else {
+                viperslide.setPower(0);
+            }
+            
 
-                if (gamepad1.dpad_left) {
+            if (gamepad1.x) {
                     claw.setPosition(0.5);
                 }
+            if (gamepad1.y){
+                claw.setPosition(1.0);
             }
 
+            telemetry.addData("x", currentPosition.getX());
+            telemetry.addData("y", currentPosition.getY());
+            telemetry.addData("rotation", heading);
+            telemetry.addData("OPOD L", opodL.getCurrentPosition());
+            telemetry.addData("OPOD C", opodC.getCurrentPosition());
+            telemetry.addData("OPOD R", opodR.getCurrentPosition());
+            telemetry.update();
+
+
+            if (gamepad1.b) {
+                threeWheelOdometryTracker.reset();
+                teleOpController.resetIMU();
+            }
             //telemetry.addData("Motor 0:",)
         }
     }
